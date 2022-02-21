7e36280cf7b23efee86354059d7fcc4e6e17c570
Plamen Panayotov <panaiotowplamen@gmail.com>
2022-02-21 17:31:20 +0200

    BalancedParentheses

--- 03. Linear-Data-Structures-Exercise-Skeleton/04.BalancedParentheses/BalancedParenthesesSolve.cs
+++ 03. Linear-Data-Structures-Exercise-Skeleton/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -7,7 +7,112 @@
     {
         public bool AreBalanced(string parentheses)
         {
-            throw new NotImplementedException();
+            Stack<char> result = new Stack<char>();
+
+            var isBalanced = true;
+
+            foreach (char item in parentheses)
+            {
+                if (result.Count == 0)
+                {
+                    result.Push(item);
+                }
+                else
+                {
+                    char lastItem = result.Peek();
+
+                    switch (lastItem)
+                    {
+                        case '(': 
+                            {
+                                switch (item)
+                                {
+                                    case ')':
+                                        var removedItem = result.Pop();
+                                        break;
+                                    case '}':
+                                        isBalanced = false;
+                                        break;
+                                    case ']':
+                                        isBalanced = false;
+                                        break;
+                                    case '(':
+                                        result.Push(item);
+                                        break;
+                                    case '{':
+                                        result.Push(item);
+                                        break;
+                                    case '[':
+                                        result.Push(item);
+                                        break;
+                                }
+                            }
+                            break;
+                        case '{':
+                            {
+                                switch (item)
+                                {
+                                    case ')':
+                                        isBalanced = false;
+                                        break;
+                                    case '}':
+                                        var removedItem = result.Pop();
+                                        break;
+                                    case ']':
+                                        isBalanced = false;
+                                        break;
+                                    case '(':
+                                        result.Push(item);
+                                        break;
+                                    case '{':
+                                        result.Push(item);
+                                        break;
+                                    case '[':
+                                        result.Push(item);
+                                        break;
+                                }
+                            }
+                            break;
+                        case '[':
+                            {
+                                switch (item)
+                                {
+                                    case ')':
+                                        isBalanced = false;
+                                        break;
+                                    case '}':
+                                        isBalanced = false;
+                                        break;
+                                    case ']':
+                                        var removedItem = result.Pop();
+                                        break;
+                                    case '(':
+                                        result.Push(item);
+                                        break;
+                                    case '{':
+                                        result.Push(item);
+                                        break;
+                                    case '[':
+                                        result.Push(item);
+                                        break;
+                                }
+                            }
+                            break;
+                    }
+                   
+                }
+                
+            }
+
+            if (isBalanced && result.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }            
+
         }
     }
 }
