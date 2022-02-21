0d1ef09048eb64674c15ae7dad27c12c799bfc41
Plamen Panayotov <panaiotowplamen@gmail.com>
2022-02-21 17:07:43 +0200

    Reverserd List

--- 03. Linear-Data-Structures-Exercise-Skeleton/03.ReversedList/ReversedList.cs
+++ 03. Linear-Data-Structures-Exercise-Skeleton/03.ReversedList/ReversedList.cs
@@ -25,11 +25,15 @@
         {
             get
             {
-                throw new NotImplementedException();
+                this.ValidateIndex(index);
+
+                return this.items[this.Count - 1 - index];
             }
             set
             {
-                throw new NotImplementedException();
+                this.ValidateIndex(index);
+
+                this.items[index] = value;
             }
         }
 
@@ -37,37 +41,86 @@
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            GrowIfNecessary();
+
+            this.items[this.Count++] = item;
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            if (IndexOf(item) != -1)
+            {
+                return true;
+            }
+
+            return false;
         }
 
         public int IndexOf(T item)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this.items[this.Count - 1 - i].Equals(item))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
 
         public void Insert(int index, T item)
         {
-            throw new NotImplementedException();
+            this.ValidateIndex(index);
+
+            GrowIfNecessary();
+
+            index = this.Count - 1 - index;
+
+            for (int i = index; i < this.Count; i++)
+            {
+                this.items[i] = this.items[i + 1];
+            }
+
+            this.items[index] = item;
+
+            Count++;
+
         }
 
         public bool Remove(T item)
         {
-            throw new NotImplementedException();
+            var index = this.IndexOf(item);
+
+            if (index != -1)
+            {
+                this.RemoveAt(index);
+               
+                return true;
+            }
+
+            return false;
         }
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            this.ValidateIndex(index);
+
+            index = this.Count - 1 - index;
+
+            for (int i = index; i < this.Count; i++)
+            {
+                this.items[i] = this.items[i + 1];
+            }
+
+            this.items[this.Count - 1] = default;
+
+            this.Count--;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            for (int i = 0; i < this.Count; i++)
+            for (int i = this.Count - 1; i >= 0; i--)
             {
                 yield return this.items[i];
             }
@@ -76,6 +129,31 @@
         IEnumerator IEnumerable.GetEnumerator()
         {
             return this.GetEnumerator();
+        }
+
+        private void GrowIfNecessary()
+        {
+            if (this.Count == this.items.Length)
+            {
+                this.items = Grow();
+            }
+        }
+
+        private T[] Grow()
+        {
+            var newArray = new T[this.Count * 2];
+
+            Array.Copy(this.items, newArray, this.items.Length);
+
+            return newArray;
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new IndexOutOfRangeException(nameof(index));
+            }
         }
     }
 }
