4250faac8b2e97e3e566667645f777d3e183076a
Plamen Panayotov <panaiotowplamen@gmail.com>
2022-02-23 16:46:32 +0200

    bfs and dfs

--- 04.Trees-Representation-and-Traversal-(BFS-DFS)-Skeleton/Tree/Tree.cs
+++ 04.Trees-Representation-and-Traversal-(BFS-DFS)-Skeleton/Tree/Tree.cs
@@ -5,13 +5,25 @@
 
     public class Tree<T> : IAbstractTree<T>
     {
+        private readonly List<Tree<T>> children;
+        private T value;
+        private Tree<T> parent;
+
         public Tree(T value)
         {
+            this.value = value;
+
+            this.children = new List<Tree<T>>();
         }
 
         public Tree(T value, params Tree<T>[] children)
             : this(value)
         {
+            foreach (var child in children)
+            {
+                child.parent = this;
+                this.children.Add(child);
+            }
         }
 
         public void AddChild(T parentKey, Tree<T> child)
@@ -21,12 +33,33 @@
 
         public IEnumerable<T> OrderBfs()
         {
-            throw new NotImplementedException();
+            var queue = new Queue<Tree<T>>();
+            var result = new List<T>();
+
+            queue.Enqueue(this);
+
+            while (queue.Count > 0)
+            {
+                var subtree = queue.Dequeue();
+
+                result.Add(subtree.value);
+
+                foreach (var child in subtree.children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return result;
         }
 
         public IEnumerable<T> OrderDfs()
         {
-            throw new NotImplementedException();
+            var list = new List<T>();
+
+            this.Dfs(this, list);
+
+            return list;
         }
 
         public void RemoveNode(T nodeKey)
@@ -37,6 +70,16 @@
         public void Swap(T firstKey, T secondKey)
         {
             throw new NotImplementedException();
+        }
+
+        private void Dfs(Tree<T> node, ICollection<T> result)
+        {
+            foreach (var child in node.children)
+            {
+                this.Dfs(child, result);
+            }
+
+            result.Add(node.value);
         }
     }
 }
